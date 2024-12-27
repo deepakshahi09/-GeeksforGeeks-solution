0e4dc27e5a82af307cd95bdaa399d90b8b889691
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2024-12-27 19:12:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find the median/find-the-median.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findMedian(vector<int> &arr) {
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        
+        if(n%2!=0){
+            return arr[n/2];
+        }
+        return (arr[n/2]+arr[n/2-1])/2;
+        // code here.
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        vector<int> arr;
+        while (ss >> num)
+            arr.push_back(num);
+
+        Solution ob;
+        cout << ob.findMedian(arr) << endl;
+    }
+}
+
+// } Driver Code Ends
