d1b3b6a036a75836eee93abacbcb9f1ac87cc59b
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-07-16 11:32:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -1,67 +1,31 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-  public:
-    void dfsHelper(int node, vector<vector<int>> &adj, vector<bool> &visited, vector<int> &ans) {
-        visited[node] = true;
-        ans.push_back(node);
-        for (int j = 0; j < adj[node].size(); j++) {
-            if (!visited[adj[node][j]]) {
-                dfsHelper(adj[node][j], adj, visited, ans);
+public:
+    vector<int> dfs(vector<vector<int>>& adj) {
+        int n = adj.size();             // Number of nodes
+        vector<bool> visited(n, false); // To mark visited nodes
+        vector<int> result;             // To store DFS traversal
+        stack<int> st;
+        
+        st.push(0); // Starting from node 0
+
+        while (!st.empty()) {
+            int node = st.top();
+            st.pop();
+
+            if (!visited[node]) {
+                visited[node] = true;
+                result.push_back(node);
+
+                // Push neighbors in reverse order for consistent traversal
+                for (int i = adj[node].size() - 1; i >= 0; i--) {
+                    int neighbor = adj[node][i];
+                    if (!visited[neighbor]) {
+                        st.push(neighbor);
+                    }
+                }
             }
         }
-    }
 
-    vector<int> dfs(vector<vector<int>>& adj) {
-        int v = adj.size();
-        vector<bool> visited(v, false);
-        vector<int> ans;
-        dfsHelper(0, adj, visited, ans);
-        return ans;
+        return result;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    cin.ignore();
-    while (tc--) {
-        int V;
-        cin >> V;
-        cin.ignore();
-        vector<vector<int>> adj(
-            V); // Use vector of vectors instead of array of vectors.
-
-        for (int i = 0; i < V; i++) {
-            string input;
-            getline(cin, input);
-            int num;
-            vector<int> node;
-            stringstream ss(input);
-            while (ss >> num) {
-                node.push_back(num);
-            }
-            adj[i] = node;
-        }
-
-        Solution obj;
-        vector<int> ans = obj.dfs(adj);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
