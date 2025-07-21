0a787dd0b83e4009dbec9377afb89f5477492d91
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-07-21 11:06:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Kth from End of Linked List/kth-from-end-of-linked-list.cpp
+++ Difficulty: Easy/Kth from End of Linked List/kth-from-end-of-linked-list.cpp
@@ -1,100 +1,28 @@
-//{ Driver Code Starts
-// Initial Template for C++
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    struct Node* next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-void printList(Node* node) {
-    while (node != NULL) {
-        cout << node->data << " ";
-        node = node->next;
-    }
-    cout << "\n";
-}
-
-
-// } Driver Code Ends
-/* struct Node {
-  int data;
-  struct Node *next;
-  Node(int x) {
-    data = x;
-    next = NULL;
-  }
-};*/
-
-// Function to find the data of kth node from the end of a linked list.
 class Solution {
   public:
     int getKthFromLast(Node *head, int k) {
-        
-        
-        Node* fast = head;
-        Node* slow = head;
-        
-        for(int i=0;i<k;i++){
-            if(fast==NULL) return -1;
-            fast = fast->next;
+        int tc = 0;
+        Node* temp = head;
+
+        // Count total nodes
+        while (temp) {
+            tc++;
+            temp = temp->next;
         }
-        
-        while(fast){
-            fast = fast->next;
-            slow = slow->next;
+
+        // Invalid k check
+        if (k > tc || k <= 0) {
+            return -1;
         }
-        return slow->data;
-          
-        // Your code here
+
+        temp = head;
+        int pos = tc - k;
+
+        // Move to (tc - k)th node
+        while (pos--) {
+            temp = temp->next;
+        }
+
+        return temp->data;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-
-        // Read numbers from the input line
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        int x;
-        cin >> x;
-        cin.ignore();
-
-        Node* head = nullptr;
-
-        // Check if the array is empty
-        if (!arr.empty()) {
-            head = new Node(arr[0]);
-            Node* tail = head;
-            for (size_t i = 1; i < arr.size(); ++i) {
-                tail->next = new Node(arr[i]);
-                tail = tail->next;
-            }
-        }
-
-        Solution ob;
-        cout << ob.getKthFromLast(head, x) << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
