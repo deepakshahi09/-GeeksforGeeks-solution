9b6f233661493c8399a77b42ef707c31667aef2e
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-07-21 10:53:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Node at a given index in linked list/node-at-a-given-index-in-linked-list.cpp
+++ Difficulty: Easy/Node at a given index in linked list/node-at-a-given-index-in-linked-list.cpp
@@ -1,27 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    struct Node *next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-void printList(Node *node) {
-    while (node != NULL) {
-        cout << node->data << " ";
-        node = node->next;
-    }
-    cout << "\n";
-}
-
-
-// } Driver Code Ends
 /* Print he nth node in the linked list Node is defined as
 /* Link list node
 struct node
@@ -42,52 +18,19 @@ struct node
 class Solution {
   public:
     int GetNth(Node *head, int index) {
-        int count = 1;
-        Node *temp = head;
-        
-        while(temp!=NULL){
-            if(count==index){
+        Node* temp = head;
+        int c = 1;
+        while(temp){
+            if(c==index){
                 return temp->data;
             }
+            c++;
             temp = temp->next;
-            count++;
             
         }
         return -1;
+        
         // Code Here
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        int k;
-        cin >> k;
-        cin.ignore();
-        struct Node *head = new Node(arr[0]);
-        struct Node *tail = head;
-        for (int i = 1; i < arr.size(); ++i) {
-            tail->next = new Node(arr[i]);
-            tail = tail->next;
-        }
-        Solution ob;
-        int res = ob.GetNth(head, k);
-        cout << res << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
