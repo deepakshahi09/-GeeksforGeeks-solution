e0e7ecb1eda90e9727533aaaf6fcff6ec2e3bd31
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-07-21 10:17:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count Linked List Nodes/count-linked-list-nodes.cpp
+++ Difficulty: Basic/Count Linked List Nodes/count-linked-list-nodes.cpp
@@ -1,19 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    struct Node *next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-
-// } Driver Code Ends
 /* Link list node */
 /*
 struct Node
@@ -27,45 +11,17 @@ class Solution {
   public:
     // Function to count nodes of a linked list.
     int getCount(struct Node* head) {
-        int nodecount = 0;
-        
-        Node* temp=head;
-        while(temp!=NULL){
-            nodecount++;
-            temp = temp->next;
+        int c= 0;
+        if(head == NULL){
+            return c;
         }
-        return nodecount;
+        while(head){
+            c++;
+            head = head->next;
+        }
+        return c;
 
         // Code here
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        struct Node *head = new Node(arr[0]);
-        struct Node *tail = head;
-        for (int i = 1; i < arr.size(); ++i) {
-            tail->next = new Node(arr[i]);
-            tail = tail->next;
-        }
-        Solution ob;
-        cout << ob.getCount(head) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
