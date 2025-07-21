062f3afba958a57ebbc815bd6abd117d82c12030
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-07-21 09:11:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check Equal Arrays/check-equal-arrays.cpp
+++ Difficulty: Easy/Check Equal Arrays/check-equal-arrays.cpp
@@ -4,10 +4,22 @@ class Solution {
   public:
     // Function to check if two arrays are equal or not.
     bool checkEqual(vector<int>& a, vector<int>& b) {
-        sort(a.begin(),a.end());
-        sort(b.begin(),b.end());
-        
-        return a==b;
+        if(a.size() != b.size()){
+            return false;
+        }
+        unordered_map<int,int>freq;
+        for(int val: a){
+            freq[val]++;
+        }
+        for(int val : b){
+            if(freq[val] == 0){
+                return false;
+            }
+            else{
+                freq[val]--;
+            }
+        }
+        return true;
         // code here
         
     }
