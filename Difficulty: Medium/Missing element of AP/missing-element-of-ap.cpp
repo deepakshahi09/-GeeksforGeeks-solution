4461113e5a2b21882a750b9d37e1be62b8dbee59
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-07-21 09:20:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Missing element of AP/missing-element-of-ap.cpp
+++ Difficulty: Medium/Missing element of AP/missing-element-of-ap.cpp
@@ -1,15 +1,16 @@
+// User function template for C++
+
 class Solution {
   public:
-    int findMissing(vector<int>& arr) {
+    int findMissing(vector<int> &arr) {
+        // code here
         int n = arr.size();
-        int l = arr[1] - arr[0];
-
-        for (int i = 2; i < n; i++) {
-            if (arr[i - 1] != arr[i] - l) {
-                return arr[i] - l;
+        int l = arr[1]-arr[0];
+        for(int i=2;i<n;i++){
+            if(arr[i]-l != arr[i-1]){
+                return arr[i]-l;
             }
         }
-
-        return arr[n - 1] + l;
+        return arr[n-1] + l;
     }
 };
