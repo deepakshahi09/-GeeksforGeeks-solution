484a224142979929301412981cf0c2fe3cc902e1
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-09-03 22:25:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Search/array-search.cpp
+++ Difficulty: Basic/Array Search/array-search.cpp
@@ -1,17 +1,17 @@
 class Solution {
   public:
     int search(vector<int>& arr, int x) {
-        int c = 0;
-        for(int i : arr){
-            if(i == x){
-                return c;
+        // code here
+        int p = 0;
+        for(int i: arr){
+            if(i==x){
+                return p;
             }
             else{
-                c++;
+                p++;
             }
         }
         return -1;
-        // code here
         
     }
 };
