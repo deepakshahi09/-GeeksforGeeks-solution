a308203ccbddd4133785d724cb09af81c6349287
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-09-04 13:54:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Binary Array Sorting/binary-array-sorting.cpp
+++ Difficulty: Medium/Binary Array Sorting/binary-array-sorting.cpp
@@ -2,6 +2,18 @@ class Solution {
   public:
     void binSort(vector<int> &arr) {
         // code here
-        sort(arr.begin(),arr.end());
+        int n = arr.size();
+        int zerocou = 0;
+        for(int i : arr){
+            if(i==0){
+                zerocou++;
+            }
+        }
+        for(int i=0;i<zerocou;i++){
+            arr[i] = 0;
+        }
+        for(int i=zerocou;i<n;i++){
+            arr[i] = 1;
+        }
     }
 };
