4e949bcd3570759383922ef68b9f003893cd4344
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-09-09 15:21:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Print Linked List/print-linked-list.cpp
+++ Difficulty: Basic/Print Linked List/print-linked-list.cpp
@@ -1,83 +1,32 @@
-//{ Driver Code Starts
-#include <iostream>
-#include <sstream>
-#include <vector>
-using namespace std;
-
-struct Node {
-    int data;
-    struct Node* next;
-
-    Node(int x) {
-        data = x;
-        next = nullptr;
-    }
-};
-
-
-// } Driver Code Ends
 /*
-struct Node {
+class Node {
+  public:
     int data;
-    struct Node* next;
+    Node* next;
 
-    Node(int x) {
-        data = x;
-        next = nullptr;
+    // Default constructor
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
     }
 };
 */
-/*
-    Print elements of a linked list on console
-    Head pointer input could be NULL as well for empty list
-*/
-
 class Solution {
   public:
-    // Function to display the elements of a linked list in same line
-    void printList(Node *head) {
+    vector<int> printList(Node *head) {
+        // code here
+        vector<int>ans;
         Node* temp = head;
-        while(temp!=NULL){
-            cout<<temp->data<<" ";
-            temp=temp->next;
+        while(temp){
+            ans.push_back(temp->data);
+            temp = temp->next;
         }
-        // your code goes here
+        return ans;
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore the newline character after t
-
-    while (t--) {
-        string input;
-        getline(cin, input); // Read the entire line for the array elements
-
-        stringstream ss(input);
-        Node *head = nullptr, *tail = nullptr;
-        int x;
-
-        // Building the linked list from input
-        while (ss >> x) {
-            if (head == nullptr) {
-                head = new Node(x);
-                tail = head;
-            } else {
-                tail->next = new Node(x);
-                tail = tail->next;
-            }
-        }
-
-        Solution ob;
-        ob.printList(head);
-        cout << endl;
-        cout << "~" << endl;
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
