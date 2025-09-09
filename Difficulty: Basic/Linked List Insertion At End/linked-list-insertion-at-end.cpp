0601f3e03af970454693a9070312bfe7c92f995d
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-09-09 15:34:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Linked List Insertion At End/linked-list-insertion-at-end.cpp
+++ Difficulty: Basic/Linked List Insertion At End/linked-list-insertion-at-end.cpp
@@ -1,26 +1,36 @@
-/*Structure of the linked list node is as
-struct Node {
-  int data;
-  struct Node * next;
-  Node(int x) {
-    data = x;
-    next = NULL;
-  }
-}; */
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
 
+    // Default constructor
+    Node() {
+        data = 0;
+        next = NULL;
+    }
+
+    // Parameterised Constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+*/
 class Solution {
   public:
     Node *insertAtEnd(Node *head, int x) {
-        Node* newNode = new Node(x);
-        if(head == NULL){
-            return newNode;
+        // Code here
+        Node* new_node = new Node(x);
+        if(head==NULL){
+            head = new_node;
+            return head;
         }
-
         Node* temp = head;
         while(temp->next){
             temp = temp->next;
         }
-        temp->next = newNode;
+        temp->next = new_node;
         return head;
     }
 };
