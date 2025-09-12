a6e093793e94b265a45e77bac007832ba21b1f2e
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-09-12 18:52:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.cpp
+++ Difficulty: Easy/Remove Duplicates from a Sorted Linked List/remove-duplicates-from-a-sorted-linked-list.cpp
@@ -1,41 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-#include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
-using namespace std;
-
-/*
-   // Redirecting input from file
-  freopen("/Users/debojyoti.mandal/expt/input.txt", "r", stdin);
-
-  // Redirecting output to file
-  freopen("/Users/debojyoti.mandal/expt/output.txt", "w", stdout);
-*/
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Node {
-    int data;
-    struct Node *next;
-
-    Node(int x) {
-        data = x;
-        next = NULL;
-    }
-};
-
-void print(Node *root) {
-    Node *temp = root;
-    while (temp != NULL) {
-        cout << temp->data << " ";
-        temp = temp->next;
-    }
-}
-
-
-// } Driver Code Ends
 /*
 struct Node {
   int data;
@@ -50,61 +12,20 @@ class Solution {
   public:
     // Function to remove duplicates from sorted linked list.
     Node* removeDuplicates(Node* head) {
-        Node*curr_node= head;
-        while(curr_node != NULL && curr_node->next != NULL){
-            if(curr_node->data == curr_node->next->data){
-                Node* temp = curr_node->next;
-                curr_node->next = curr_node->next->next;
-                delete temp;
+        if(head == NULL){
+            return head;
+        }
+        Node* temp = head;
+        while(temp && temp->next){
+            if(temp->data == temp->next->data){
+                Node* del = temp->next;
+                temp->next = temp->next->next;
+                delete del;
             }
             else{
-                curr_node = curr_node->next;
+                temp = temp->next;
             }
         }
         return head;
-
-        // code here
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-
-    // your code goes here
-    int T;
-    cin >> T;
-    cin.ignore();
-    while (T--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        if (arr.empty()) {
-            cout << -1 << endl;
-            continue;
-        }
-        Solution obj;
-        int data = arr[0];
-        Node *head = new Node(data);
-        Node *tail = head;
-        for (int i = 1; i < arr.size(); ++i) {
-            data = arr[i];
-            tail->next = new Node(data);
-            tail = tail->next;
-        }
-        Solution ob;
-        Node *result = ob.removeDuplicates(head);
-        print(result);
-        cout << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
