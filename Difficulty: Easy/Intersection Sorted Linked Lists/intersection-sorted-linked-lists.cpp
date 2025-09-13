34eae47c60a564edb4d55c891a9ff5db6be42c89
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-09-13 21:19:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection Sorted Linked Lists/intersection-sorted-linked-lists.cpp
@@ -0,0 +1,40 @@
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    Node* findIntersection(Node* head1, Node* head2) {
+        unordered_set<int>set;
+        Node* temp = head1;
+        while(temp){
+            set.insert(temp->data);
+            temp = temp->next;
+        }
+        Node* dummy = new Node(0);
+        Node* tail = dummy;
+        while(head2){
+            if(set.find(head2->data) != set.end()){
+              tail->next = new Node(head2->data);
+              tail = tail->next;
+            }
+            head2 = head2->next;
+            
+        }
+        return dummy->next;
+        
+        
+        // code goes here.
+    }
+};
