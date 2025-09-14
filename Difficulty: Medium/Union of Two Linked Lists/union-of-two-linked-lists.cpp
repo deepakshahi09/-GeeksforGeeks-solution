fdf09fa96246c9081e1517a3f2217510c19233b2
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-09-14 12:38:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of Two Linked Lists/union-of-two-linked-lists.cpp
@@ -0,0 +1,45 @@
+/*
+// structure of the node is as follows
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+
+*/
+class Solution {
+  public:
+    struct Node* makeUnion(struct Node* head1, struct Node* head2) {
+        // code here
+        unordered_set<int>set;
+        Node* temp1 = head1;
+        while(temp1){
+            set.insert(temp1->data);
+            temp1 = temp1->next;
+        }
+        Node* temp2 = head2;
+        while(temp2){
+            set.insert(temp2->data);
+            temp2=temp2->next;
+        }
+        Node* dummy = new Node(0);
+        Node* tail = dummy;
+        
+        vector<int>ans(set.begin(),set.end());
+        sort(ans.begin(),ans.end());
+        
+        for(int i : ans){
+            tail->next = new Node(i);
+            tail = tail->next;
+        }
+        return dummy->next;
+        
+    }
+};
