627a1e734b43b41ef206f9acecb4f892115a01b9
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-10-05 11:59:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Get min at pop/get-min-at-pop.cpp
+++ Difficulty: Easy/Get min at pop/get-min-at-pop.cpp
@@ -1,63 +1,24 @@
-//{ Driver Code Starts
-//Initial Template for C++
-
-
-#include <bits/stdc++.h>
-using namespace std;
-
-stack<int> _push(int arr[],int n);
-
-void _getMinAtPop(stack<int>s);
-
-// } Driver Code Ends
-//User function Template for C++
-
-
-//Function to push all the elements into the stack.
-stack<int> _push(int arr[],int n)
-{
+stack<int> _push(int arr[], int n) {
     stack<int>st;
     for(int i=0;i<n;i++){
-        if(i==0){
+        if(st.empty()){
             st.push(arr[i]);
         }
         else{
-        st.push(min(arr[i],st.top()));
+            st.push(min(arr[i],st.top()));
         }
     }
     return st;
-   // your code here
+    // code here
+    
 }
 
-//Function to print minimum value in stack each time while popping.
-void _getMinAtPop(stack<int>s)
-{
+// Function to print minimum value in stack each time while popping.
+void _getMinAtPop(stack<int> s) {
     while(!s.empty()){
         cout<<s.top()<<" ";
         s.pop();
     }
-    // your code here
+    
+    // code here
 }
-
-//{ Driver Code Starts.
-int main() {
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int n;
-	    cin>>n;
-	    int arr[n];
-	    for(int i=0;i<n;i++)
-	    cin>>arr[i];
-	    stack<int>mys=_push(arr,n);
-	    _getMinAtPop(mys);
-	    
-	    cout<<endl;
-	    
-	}
-	return 0;
-}
-
-
-// } Driver Code Ends
