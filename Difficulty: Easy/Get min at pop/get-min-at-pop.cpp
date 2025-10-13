54786de86f11aa8b0683558f9e757b17e54374db
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-10-13 17:39:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Get min at pop/get-min-at-pop.cpp
+++ Difficulty: Easy/Get min at pop/get-min-at-pop.cpp
@@ -1,7 +1,9 @@
-stack<int> _push(int arr[], int n) {
-    // code here
+// User function Template for C++
+
+// Function to push all the elements into the stack.
+stack<int> push(vector<int>& arr) {
     stack<int>st;
-    for(int i=0;i<n;i++){
+    for(int i=0;i<arr.size();i++){
         if(st.empty()){
             st.push(arr[i]);
         }
@@ -10,13 +12,16 @@ stack<int> _push(int arr[], int n) {
         }
     }
     return st;
+    // your code here
+    
 }
 
 // Function to print minimum value in stack each time while popping.
-void _getMinAtPop(stack<int> s) {
+void getMinAtPop(stack<int> s) {
     while(!s.empty()){
         cout<<s.top()<<" ";
         s.pop();
     }
-    // code here
+    
+    // your code here
 }
