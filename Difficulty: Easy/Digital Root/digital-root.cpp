7d786f391b376dc960256c8f20b3bb36e4c8bd5f
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-11-01 20:12:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Digital Root/digital-root.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int digitalRoot(int n) {
+        if(n<10){
+            return n;
+        }
+        int sum = 0;
+        while(n > 0){
+            sum+=n % 10;
+            n/=10;
+        }
+        return digitalRoot(sum);
+        // code here
+        
+    }
+};
