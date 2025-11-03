9af4227ed6aa26ebd2c21c38b389768b61e468fc
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-11-03 15:23:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Recursive sequence/recursive-sequence.cpp
@@ -0,0 +1,23 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    long long sequence(int n) {
+        // code here
+        long long ans = 0;
+        long long num = 1;
+        long long mod = 1e9+7;
+        
+        for(int i=1;i<=n;i++){
+           long long term = 1;
+           for(int j = 1;j<=i;j++){
+               term = (term * num) % mod;
+                num++;
+           }
+            ans = (ans + term) % mod;
+
+        }
+        
+        return ans;
+    }
+};
