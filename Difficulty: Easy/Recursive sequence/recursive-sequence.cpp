e219570fc84f156b2eeee99ff990696dccd1bc48
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-11-03 15:31:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Recursive sequence/recursive-sequence.cpp
+++ Difficulty: Easy/Recursive sequence/recursive-sequence.cpp
@@ -5,19 +5,16 @@ class Solution {
     long long sequence(int n) {
         // code here
         long long ans = 0;
+        long long mod = 1e9 + 7;
         long long num = 1;
-        long long mod = 1e9+7;
-        
         for(int i=1;i<=n;i++){
-           long long term = 1;
-           for(int j = 1;j<=i;j++){
-               term = (term * num) % mod;
+            long long term = 1;
+            for(int j = 1;j<=i;j++){
+                term = (term * num) % mod;
                 num++;
-           }
+            }
             ans = (ans + term) % mod;
-
         }
-        
         return ans;
     }
 };
