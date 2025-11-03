f41112f04ca54a3e9283a80577304c5f08a2ab8b
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-11-03 15:02:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insert an Element at the Bottom of a Stack/insert-an-element-at-the-bottom-of-a-stack.cpp
+++ Difficulty: Easy/Insert an Element at the Bottom of a Stack/insert-an-element-at-the-bottom-of-a-stack.cpp
@@ -1,20 +1,44 @@
 // User function Template for C++
 
+// class Solution {
+//   public:
+//     stack<int> insertAtBottom(stack<int> st, int x) {
+//         stack<int>help;
+//         while(!st.empty()){
+//             int top = st.top();
+//             help.push(top);
+//             st.pop();
+//         }
+//         st.push(x);
+//         while(!help.empty()){
+//             int t = help.top();
+//             st.push(t);
+//             help.pop();
+//         }
+//         return st;
+//     }
+// };
+
 class Solution {
-  public:
+public:
+    // Function to insert an element at the bottom of the stack
     stack<int> insertAtBottom(stack<int> st, int x) {
-        stack<int>help;
-        while(!st.empty()){
-            int top = st.top();
-            help.push(top);
-            st.pop();
+        // Base case: if stack is empty, push x and return
+        if (st.empty()) {
+            st.push(x);
+            return st;
         }
-        st.push(x);
-        while(!help.empty()){
-            int t = help.top();
-            st.push(t);
-            help.pop();
-        }
+
+        // Remove the top element
+        int top = st.top();
+        st.pop();
+
+        // Recursive call to insert x at bottom of smaller stack
+        st = insertAtBottom(st, x);
+
+        // Push the top element back after inserting x
+        st.push(top);
+
         return st;
     }
 };
