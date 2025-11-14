964b9aea2962facab0c9960fd32c4286a2ee0640
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-11-15 05:20:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.cpp
+++ Difficulty: Easy/Roman Number to Integer/roman-number-to-integer.cpp
@@ -1,82 +1,29 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-// User function template for C++
-
 class Solution {
   public:
+  int val(char c){
+            if(c == 'I') return 1;
+            if(c == 'V') return 5;
+            if(c == 'X') return 10;
+            if(c == 'L') return 50;
+            if(c == 'C') return 100;
+            if(c == 'D') return 500;
+            if(c == 'M') return 1000;
+        }
     int romanToDecimal(string &s) {
+        // code here
         
         int total = 0;
-        
-        for(int i=0;i<s.size();i++){
-            
-            if(s[i]=='I'){
-                if(s[i+1]=='V' || s[i+1]=='X'){
-                    total-=1;
-                }
-                else{
-                    total+=1;
-                }
+        int prev = 0;
+        for(int i=s.size()-1;i>=0;i--){
+            int curr = val(s[i]);
+            if(curr < prev){
+                total-=curr;
             }
-            
-            else if(s[i]=='V'){
-                total+=5;
+            else{
+                total+=curr;
             }
-            
-            else if(s[i]=='X'){
-                if(s[i+1]=='L' || s[i+1]=='C'){
-                    total-=10;
-                }
-                else{
-                    total+=10;
-                }
-            }
-            
-            else if(s[i]=='L'){
-                total+=50;
-            }
-            
-            else if(s[i]=='C'){
-                if(s[i+1]=='M' || s[i+1]=='D'){
-                    total-=100;
-                }
-                else{
-                    total+=100;
-                }
-            }
-            
-            else if(s[i]=='D'){
-                total+=500;
-            }
-            
-            else if(s[i]=='M'){
-                total+=1000;
-            }
+            prev = curr;
         }
         return total;
-        // code here
     }
 };
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        string s;
-        cin >> s;
-        Solution ob;
-        cout << ob.romanToDecimal(s) << endl;
-
-        cout << "~"
-             << "\n";
-    }
-}
-// } Driver Code Ends
