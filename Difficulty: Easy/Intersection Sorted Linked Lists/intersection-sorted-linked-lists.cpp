25cb977ff232eb22bac8ae748388f45da872251b
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2025-11-21 06:26:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Intersection Sorted Linked Lists/intersection-sorted-linked-lists.cpp
+++ Difficulty: Easy/Intersection Sorted Linked Lists/intersection-sorted-linked-lists.cpp
@@ -16,21 +16,66 @@ struct Node
 class Solution {
   public:
     Node* findIntersection(Node* head1, Node* head2) {
+        // unordered_set<int>set;
+        // Node* temp = head1;
+        // while(temp){
+        //     set.insert(temp->data);
+        //     temp = temp->next;
+        // }
+        // Node* dummy = new Node(0);
+        // Node* tail = dummy;
+        // while(head2){
+        //     if(set.find(head2->data) != set.end()){
+        //       tail->next = new Node(head2->data);
+        //       tail = tail->next;
+        //     }
+        //     head2 = head2->next;
+            
+        // }
+        // return dummy->next;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         unordered_set<int>set;
-        Node* temp = head1;
-        while(temp){
-            set.insert(temp->data);
-            temp = temp->next;
+        while(head1){
+            set.insert(head1->data);
+            head1 = head1->next;
         }
+        
         Node* dummy = new Node(0);
-        Node* tail = dummy;
+        Node* temp = dummy; 
         while(head2){
             if(set.find(head2->data) != set.end()){
-              tail->next = new Node(head2->data);
-              tail = tail->next;
+                temp->next = new Node(head2->data);
+                temp = temp->next;
             }
             head2 = head2->next;
-            
         }
         return dummy->next;
         
