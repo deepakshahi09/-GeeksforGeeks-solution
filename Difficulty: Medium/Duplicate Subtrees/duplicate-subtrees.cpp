167b4f03dac8fe94d62035f284ecffcb4c661e68
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-01-19 16:00:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Duplicate Subtrees/duplicate-subtrees.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    unordered_map<string, int>mp;
+    vector<Node*>ans;
+  
+    string solve(Node* root){
+        if(root == NULL){
+            return "#";
+        }
+        string left = solve(root->left);
+        string right = solve(root->right);
+        
+        string s = to_string(root->data) + "," + left + "," + right;
+        
+        if(mp[s] == 1){
+            ans.push_back(root);
+        }
+        mp[s]++;
+        return s;
+    }
+    vector<Node*> printAllDups(Node* root) {
+        mp.clear();
+        ans.clear();
+        solve(root);
+        return ans;
+        // Code here
+        
+    }
+};
