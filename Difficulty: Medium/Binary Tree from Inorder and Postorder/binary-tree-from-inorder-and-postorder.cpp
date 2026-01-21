3d10a7bc6603d659a2e201a32bde0676ab5fbe08
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-01-21 12:47:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Binary Tree from Inorder and Postorder/binary-tree-from-inorder-and-postorder.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int postIndex;
+
+    Node* solve(vector<int> &inorder, vector<int> &postorder, int start, int end) {
+        if (start > end)
+            return NULL;
+
+        // Create root from postorder
+        Node* root = new Node(postorder[postIndex--]);
+
+        // Find root position in inorder
+        int pos;
+        for (int i = start; i <= end; i++) {
+            if (inorder[i] == root->data) {
+                pos = i;
+                break;
+            }
+        }
+
+        // Build right first, then left
+        root->right = solve(inorder, postorder, pos + 1, end);
+        root->left  = solve(inorder, postorder, start, pos - 1);
+
+        return root;
+    }
+
+    Node *buildTree(vector<int> &inorder, vector<int> &postorder) {
+        postIndex = postorder.size() - 1;
+        return solve(inorder, postorder, 0, inorder.size() - 1);
+    }
+};
