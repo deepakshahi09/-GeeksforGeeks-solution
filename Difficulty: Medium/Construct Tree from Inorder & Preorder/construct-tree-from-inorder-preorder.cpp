5092759bd715a3f5260d4ebbbdad16c5450904c7
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-01-21 12:15:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Construct Tree from Inorder & Preorder/construct-tree-from-inorder-preorder.cpp
+++ Difficulty: Medium/Construct Tree from Inorder & Preorder/construct-tree-from-inorder-preorder.cpp
@@ -1,32 +1,41 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
 class Solution {
   public:
     int preIndex = 0;
-
-    Node* solve(vector<int> &inorder, vector<int> &preorder, int start, int end) {
-        if (start > end)
+    Node* solve(vector<int>&inorder, vector<int>&preorder,int st, int lst){
+        if(st > lst){
             return NULL;
-
-        // Create root from preorder
+        }
         Node* root = new Node(preorder[preIndex++]);
-
-        // Find root position in inorder
         int pos;
-        for (int i = start; i <= end; i++) {
-            if (inorder[i] == root->data) {
+        for(int i = st;i<=lst;i++){
+            if(inorder[i] == root->data){
                 pos = i;
                 break;
             }
         }
-
-        // Build left and right subtrees
-        root->left = solve(inorder, preorder, start, pos - 1);
-        root->right = solve(inorder, preorder, pos + 1, end);
-
+        root->left = solve(inorder,preorder,st,pos-1);
+        root->right = solve(inorder,preorder,pos+1,lst);
         return root;
     }
-
     Node *buildTree(vector<int> &inorder, vector<int> &preorder) {
         preIndex = 0;
-        return solve(inorder, preorder, 0, inorder.size() - 1);
+        solve(inorder,preorder,0,inorder.size()-1);
+        // code here
+        
     }
 };
