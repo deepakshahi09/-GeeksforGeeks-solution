28939a7095066529dd98d70f3673d4c49319460b
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-01-22 13:22:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Iterative Inorder/iterative-inorder.cpp
+++ Difficulty: Medium/Iterative Inorder/iterative-inorder.cpp
@@ -6,37 +6,77 @@ struct Node {
     Node* left;
     Node* right;
 };*/
-class Solution {
-  public:
-    vector<int> inOrder(Node* root) {
+// class Solution {
+//   public:
+//     vector<int> inOrder(Node* root) {
         // code here
-        stack<Node*>st;
-        stack<bool>visit;
-        vector<int>ans;
-        st.push(root);
-        visit.push(0);
-        while(!st.empty()){
-            Node* temp = st.top();
-            st.pop();
-            bool flag = visit.top();
-            visit.pop();
+        // stack<Node*>st;
+        // stack<bool>visit;
+        // vector<int>ans;
+        // st.push(root);
+        // visit.push(0);
+        // while(!st.empty()){
+        //     Node* temp = st.top();
+        //     st.pop();
+        //     bool flag = visit.top();
+        //     visit.pop();
             
-            if(flag == 0){
-                if(temp->right){
-                    st.push(temp->right);
-                    visit.push(0);
-                }
-                st.push(temp);
-                visit.push(1);
-                if(temp->left){
-                    st.push(temp->left);
-                    visit.push(0);
-                }
+        //     if(flag == 0){
+        //         if(temp->right){
+        //             st.push(temp->right);
+        //             visit.push(0);
+        //         }
+        //         st.push(temp);
+        //         visit.push(1);
+        //         if(temp->left){
+        //             st.push(temp->left);
+        //             visit.push(0);
+        //         }
+        //     }
+        //     else{
+        //         ans.push_back(temp->data);
+        //     }
+        // }
+        // return ans;
+        
+        
+        
+        class Solution {
+public:
+    vector<int> inOrder(Node* root) {
+        vector<int> ans;
+
+        while (root != NULL) {
+
+            // Case 1: Left child does not exist
+            if (root->left == NULL) {
+                ans.push_back(root->data);
+                root = root->right;
             }
-            else{
-                ans.push_back(temp->data);
+            // Case 2: Left child exists
+            else {
+                Node* curr = root->left;
+
+                // Find inorder predecessor
+                while (curr->right != NULL && curr->right != root) {
+                    curr = curr->right;
+                }
+
+                // Left subtree not traversed
+                if (curr->right == NULL) {
+                    curr->right = root;     // make temporary link
+                    root = root->left;
+                }
+                // Left subtree already traversed
+                else {
+                    curr->right = NULL;     // remove link
+                    ans.push_back(root->data);
+                    root = root->right;
+                }
             }
         }
         return ans;
     }
 };
+//     }
+// };
