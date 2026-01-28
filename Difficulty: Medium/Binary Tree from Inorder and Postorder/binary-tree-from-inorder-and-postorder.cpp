5d2504eb7dba8edebaf4f855356d8034e4707617
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-01-28 11:50:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Binary Tree from Inorder and Postorder/binary-tree-from-inorder-and-postorder.cpp
+++ Difficulty: Medium/Binary Tree from Inorder and Postorder/binary-tree-from-inorder-and-postorder.cpp
@@ -1,32 +1,47 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = nullptr;
+    }
+};
+*/
+
 class Solution {
   public:
-    int postIndex;
-
-    Node* solve(vector<int>& inorder, vector<int>& postorder, int st, int lst) {
-        if (st > lst)
+  int postindex;
+    Node* solve(vector<int> &inorder, vector<int> &postorder, int st, int lst){
+        if(st > lst){
             return NULL;
-
-        // Create root from postorder
-        Node* root = new Node(postorder[postIndex--]);
-
-        // Find root in inorder
+        }
+        Node* root = new Node(postorder[postindex--]);
+        
         int pos;
-        for (int i = st; i <= lst; i++) {
-            if (inorder[i] == root->data) {
+        for(int i = st;i<=lst;i++){
+            if(inorder[i] == root->data){
                 pos = i;
                 break;
             }
         }
-
-        // Build right first, then left
-        root->right = solve(inorder, postorder, pos + 1, lst);
-        root->left  = solve(inorder, postorder, st, pos - 1);
-
+        
+        root->right = solve(inorder,postorder,pos+1,lst);
+        root->left = solve(inorder,postorder,st,pos-1);
+        
+        
         return root;
+        
+        
     }
-
     Node *buildTree(vector<int> &inorder, vector<int> &postorder) {
-        postIndex = postorder.size() - 1;
-        return solve(inorder, postorder, 0, inorder.size() - 1);
+        postindex = postorder.size()-1;
+        return solve(inorder,postorder,0,inorder.size()-1);
+        
+        // code here
+        
     }
 };
