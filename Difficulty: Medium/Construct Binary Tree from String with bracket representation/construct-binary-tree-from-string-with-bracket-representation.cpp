be0c2423fdc3f45a90d9aaeb9c77ee43a48b1fff
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-01-28 09:55:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Construct Binary Tree from String with bracket representation/construct-binary-tree-from-string-with-bracket-representation.cpp
+++ Difficulty: Medium/Construct Binary Tree from String with bracket representation/construct-binary-tree-from-string-with-bracket-representation.cpp
@@ -1,40 +1,51 @@
+// User function Template for C++
+
+/*
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = this->right = NULL;
+    }
+};
+*/
+
 class Solution {
   public:
-    
-    Node* solve(string &s, int &i) {
-        // If empty subtree
-        if (s[i] == ')') {
+    Node* solve(string &s,int &i){
+        if(s[i] == ')'){
             return NULL;
         }
-        
-        // Read number
         int num = 0;
-        while (i < s.size() && isdigit(s[i])) {
-            num = num * 10 + (s[i] - '0');
+      
+        while(i<s.size() && isdigit(s[i])){
+            num = num*10 + (s[i]-'0');
             i++;
         }
-        
         Node* root = new Node(num);
-        
-        // Left child
-        if (i < s.size() && s[i] == '(') {
-            i++; // skip '('
-            root->left = solve(s, i);
-            i++; // skip ')'
+        if(i<s.size() && s[i] == '('){
+            i++;
+            root->left  = solve(s,i);
+            i++;
         }
-        
-        // Right child
-        if (i < s.size() && s[i] == '(') {
-            i++; // skip '('
-            root->right = solve(s, i);
-            i++; // skip ')'
+        if(i<s.size() && s[i] == '('){
+            i++;
+            root->right = solve(s,i);
+            i++;
         }
-        
         return root;
+        
+        
     }
-    
-    Node* treeFromString(string str) {
+    // function to construct tree from string
+    Node *treeFromString(string str) {
         int i = 0;
-        return solve(str, i);
+        solve(str,i);
+        // code here
+        
     }
 };
