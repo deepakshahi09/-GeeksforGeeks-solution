7288db590c6e0cc8ebfdba0083a3a802c55c7180
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-02-05 23:28:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Inorder traversal of a BST/inorder-traversal-of-a-bst.cpp
@@ -0,0 +1,16 @@
+
+    void inorder(Node* root, vector<int>& ans) {
+        if (root == NULL)
+            return;
+
+        inorder(root->left, ans);
+        ans.push_back(root->data);
+        inorder(root->right, ans);
+    }
+
+    vector<int> inOrder(Node* root) {
+        vector<int> ans;
+        inorder(root, ans);
+        return ans;
+    }
+
