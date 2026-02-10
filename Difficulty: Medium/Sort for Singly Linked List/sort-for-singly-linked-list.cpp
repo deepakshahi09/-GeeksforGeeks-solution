15c6b078bd1b0aff013c55d268ab5d900f228e94
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-02-11 00:36:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort for Singly Linked List/sort-for-singly-linked-list.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    Node* merge(Node* h1, Node* h2){
+        if(!h1) return h2;
+        if(!h2) return h1;
+
+        if(h1->data <= h2->data){
+            h1->next = merge(h1->next, h2);
+            return h1;
+        } else {
+            h2->next = merge(h1, h2->next);
+            return h2;
+        }
+    }
+
+    Node* getmid(Node* head){
+        Node* slow = head;
+        Node* fast = head->next;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    Node* insertionSort(Node* head){
+        if(!head || !head->next)
+            return head;
+
+        Node* mid = getmid(head);
+        Node* right = mid->next;
+        mid->next = NULL;
+
+        Node* left = insertionSort(head);
+        right = insertionSort(right);
+
+        return merge(left, right);
+    }
+};
