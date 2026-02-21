1c7488bf3b50d6aa39dd0dd11f6248ffb521b664
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-02-21 06:46:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -3,14 +3,17 @@ class Solution {
     int perfectSum(vector<int>& arr, int target) {
         // code here
         int n = arr.size();
-        const int mod = 1e9+7;
-        vector<vector<int>>dp(n+1,vector<int>(target+1,0));
-        dp[0][0] =1;
+        vector<vector<int>>dp(n+1,vector<int>(target+1));
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
         for(int i=1;i<=n;i++){
-            for(int j=0; j<=target;j++){
-                dp[i][j] = dp[i-1][j];
+            for(int j = 0;j<=target;j++){
                 if(arr[i-1] <= j){
-                    dp[i][j] = (dp[i][j] + dp[i-1][j-arr[i-1]]) % mod;
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-arr[i-1]];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
