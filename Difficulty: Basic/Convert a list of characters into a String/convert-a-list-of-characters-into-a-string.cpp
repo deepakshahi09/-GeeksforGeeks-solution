ee5280a226151d0eb57803c213a2d92947a56283
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-04-18 11:23:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Convert a list of characters into a String/convert-a-list-of-characters-into-a-string.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+  public:
+    string chartostr(char arr[], int n) {
+        string ans = "";
+        for(int i = 0; i<n;i++){
+            ans+=arr[i];
+        }
+        return ans;
+        // code here
+        
+    }
+};
