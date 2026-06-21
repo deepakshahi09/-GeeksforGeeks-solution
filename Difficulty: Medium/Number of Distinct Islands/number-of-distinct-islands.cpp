58a31ec7e6ec21410c2f054629f5d59c159c49f6
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-06-21 10:23:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.cpp
+++ Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.cpp
@@ -1,46 +1,108 @@
-class Solution {
-    private:
-    void dfs(int row, int col,int baseRow,int baseCol,
-                vector<vector<int>>&visit,
-                vector<vector<char>>&grid,
-                int delrow[],int delcol[],
-                vector<pair<int,int>>&shape){
+// class Solution {
+//     private:
+//     void dfs(int row, int col,int baseRow,int baseCol,
+//                 vector<vector<int>>&visit,
+//                 vector<vector<char>>&grid,
+//                 int delrow[],int delcol[],
+//                 vector<pair<int,int>>&shape){
                     
-        visit[row][col] = 1;
-        shape.push_back({row - baseRow,col - baseCol});
+//         visit[row][col] = 1;
+//         shape.push_back({row - baseRow,col - baseCol});
         
-        int n = grid.size();
-        int m = grid[0].size();
-        for(int i=0;i<4;i++){
-            int nrow = row+delrow[i];
-            int ncol = col+delcol[i];
+//         int n = grid.size();
+//         int m = grid[0].size();
+//         for(int i=0;i<4;i++){
+//             int nrow = row+delrow[i];
+//             int ncol = col+delcol[i];
             
-            if(nrow >= 0 && nrow < n && ncol >= 0 && ncol <m && !visit[nrow][ncol] && 
-            grid[nrow][ncol] == 'L'){
+//             if(nrow >= 0 && nrow < n && ncol >= 0 && ncol <m && !visit[nrow][ncol] && 
+//             grid[nrow][ncol] == 'L'){
                 
-                dfs(nrow,ncol,baseRow,baseCol,visit,grid,delrow,delcol,shape);
-            }
-        }
+//                 dfs(nrow,ncol,baseRow,baseCol,visit,grid,delrow,delcol,shape);
+//             }
+//         }
         
-    }
+//     }
+//   public:
+//     int countDistinctIslands(vector<vector<char>>& grid) {
+//         // code here
+//         set<vector<pair<int,int>>>st;
+//         int delrow[] = {-1,0,1,0};
+//         int delcol[] = {0,1,0,-1};
+//         int n = grid.size();
+//         int m = grid[0].size();
+//         vector<vector<int>>visit(n,vector<int>(m,0));
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(!visit[i][j] && grid[i][j]=='L'){
+//                     vector<pair<int,int>>shape;
+//                     dfs(i,j,i,j,visit,grid,delrow,delcol,shape);
+//                     st.insert(shape);
+//                 }
+//             }
+//         }
+//         return st.size();
+//     }
+// };
+
+
+
+class Solution {
   public:
     int countDistinctIslands(vector<vector<char>>& grid) {
-        // code here
-        set<vector<pair<int,int>>>st;
-        int delrow[] = {-1,0,1,0};
-        int delcol[] = {0,1,0,-1};
+        
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>>visit(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!visit[i][j] && grid[i][j]=='L'){
-                    vector<pair<int,int>>shape;
-                    dfs(i,j,i,j,visit,grid,delrow,delcol,shape);
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        set<vector<pair<int,int>>> st;
+
+        int delRow[] = {-1, 0, 1, 0};
+        int delCol[] = {0, 1, 0, -1};
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+
+                if(!vis[i][j] && grid[i][j] == 'L') {
+
+                    queue<pair<int,int>> q;
+                    vector<pair<int,int>> shape;
+
+                    q.push({i, j});
+                    vis[i][j] = 1;
+
+                    int baseRow = i;
+                    int baseCol = j;
+
+                    while(!q.empty()) {
+
+                        int row = q.front().first;
+                        int col = q.front().second;
+                        q.pop();
+
+                        shape.push_back({row - baseRow, col - baseCol});
+
+                        for(int k = 0; k < 4; k++) {
+
+                            int nrow = row + delRow[k];
+                            int ncol = col + delCol[k];
+
+                            if(nrow >= 0 && nrow < n &&
+                               ncol >= 0 && ncol < m &&
+                               !vis[nrow][ncol] &&
+                               grid[nrow][ncol] == 'L') {
+
+                                vis[nrow][ncol] = 1;
+                                q.push({nrow, ncol});
+                            }
+                        }
+                    }
+
                     st.insert(shape);
                 }
             }
         }
+
         return st.size();
     }
 };
