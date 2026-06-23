01a0f96461dc6a47ceb6234359926afbe7fff9ae
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-06-23 11:21:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Connected Components/number-of-connected-components.cpp
+++ Difficulty: Medium/Number of Connected Components/number-of-connected-components.cpp
@@ -1,36 +1,32 @@
 class Solution {
-  public:
-    void dfs(int node, vector<bool>&visit,vector<vector<int>>&adj){
-        visit[node] = true;
-        for(int i : adj[node]){
-            if(!visit[i]){
-                dfs(i,visit,adj);
+    private:
+        void dfs(int node, vector<int>&visit,vector<int>adj[]){
+            visit[node] = 1;
+            for(int i : adj[node]){
+                if(!visit[i]){
+                    dfs(i,visit,adj);
+                }
             }
         }
-    }
-    int countConnected(int v, vector<vector<int>>& edges) {
-        
-        vector<vector<int>>adj(v);
-        for(auto it : edges){
-            int u = it[0];
-            int v = it[1];
-            
+  public:
+    int countConnected(int V, vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int>adj[V];
+        for(int i=0;i<n;i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        
-        vector<bool>visit(v,false);
+        vector<int>visit(V,0);
         int count = 0;
-        
-        for(int i=0;i<v;i++){
+        for(int i=0;i<V;i++){
             if(!visit[i]){
                 count++;
                 dfs(i,visit,adj);
             }
         }
         return count;
-        
-        
         
         
     }
