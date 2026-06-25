ff5e7f983447d990cdb15b82ff003c666f75cf63
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-06-25 13:40:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Course Schedule II/course-schedule-ii.cpp
+++ Difficulty: Medium/Course Schedule II/course-schedule-ii.cpp
@@ -1,39 +1,126 @@
+// class Solution {
+//   public:
+//     vector<int> findOrder(int n, vector<vector<int>> &p) {
+//         vector<int>adj[n];
+//         vector<int>indegree(n,0);
+//         for(int i=0;i<p.size();i++){
+//             int u = p[i][0];
+//             int v = p[i][1];
+//             adj[v].push_back(u);
+//             indegree[v]++;
+//         }
+//         queue<int>q;
+//         for(int i=0;i<n;i++){
+//             if(indegree[i] == 0){
+//                 q.push(i);
+//             }
+//         }
+//         vector<int>ans;
+//         int count = 0;
+//         while(!q.empty()){
+//             int fr = q.front();
+//             q.pop();
+//             count++;
+            
+//             for(int i:adj[fr]){
+//                 indegree[i]--;
+//                 if(indegree[i] == 0){
+//                     q.push(i);
+//                 }
+//             }
+//         }
+//         vector<int>temp;
+//         if(count != n){
+//             return temp;
+//         }
+//         return ans;
+        
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Solution {
-  public:
+public:
+    bool dfs(int node, vector<int> adj[], vector<int> &vis,
+             vector<int> &path, vector<int> &ans) {
+
+        vis[node] = 1;
+        path[node] = 1;
+
+        for(int it : adj[node]){
+
+            if(!vis[it]){
+                if(dfs(it, adj, vis, path, ans))
+                    return true;
+            }
+            else if(path[it]){
+                return true;
+            }
+        }
+
+        path[node] = 0;
+        ans.push_back(node);
+
+        return false;
+    }
+
     vector<int> findOrder(int n, vector<vector<int>> &p) {
-        vector<int>adj[n];
-        vector<int>indegree(n,0);
+
+        vector<int> adj[n];
+
         for(int i=0;i<p.size();i++){
             int u = p[i][0];
             int v = p[i][1];
+
             adj[v].push_back(u);
-            indegree[v]++;
         }
-        queue<int>q;
+
+        vector<int> vis(n,0);
+        vector<int> path(n,0);
+        vector<int> ans;
+
         for(int i=0;i<n;i++){
-            if(indegree[i] == 0){
-                q.push(i);
+            if(!vis[i]){
+                if(dfs(i, adj, vis, path, ans))
+                    return {};
             }
         }
-        vector<int>ans;
-        int count = 0;
-        while(!q.empty()){
-            int fr = q.front();
-            q.pop();
-            count++;
-            
-            for(int i:adj[fr]){
-                indegree[i]--;
-                if(indegree[i] == 0){
-                    q.push(i);
-                }
-            }
-        }
-        vector<int>temp;
-        if(count != n){
-            return temp;
-        }
+
+        reverse(ans.begin(), ans.end());
+
         return ans;
-        
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
