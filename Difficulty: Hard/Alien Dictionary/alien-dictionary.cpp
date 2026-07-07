6f62f98612dfb71da2c486540598997a90d0c22d
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-07-07 10:30:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.cpp
@@ -2,60 +2,62 @@ class Solution {
   public:
     string findOrder(vector<string> &words) {
         vector<int>adj[26];
-        vector<int>pa(26,0);
         vector<int>indegree(26,0);
-        
-        for(string s : words){
-            for(char ch : s){
-                pa[ch-'a']++;
+        vector<int>paresent(26,0);
+        for(auto it : words){
+            for(char ch : it){
+                paresent[ch -'a']++;
             }
         }
+        
         for(int i=0;i<words.size()-1;i++){
             string s1 = words[i];
             string s2 = words[i+1];
-            
+            bool found = false;
             int len = min(s1.size(),s2.size());
-            bool fount = false;
             for(int j=0;j<len;j++){
                 if(s1[j] != s2[j]){
+                    found = true;
                     adj[s1[j] -'a'].push_back(s2[j]-'a');
-                    indegree[s2[j]-'a']++;
-                    fount = true;
+                    indegree[s2[j] -'a']++;
                     break;
                 }
             }
-            if(!fount && s1.size() > s2.size()){
+            if(!found && s1.size() > s2.size()){
                 return "";
             }
-            
         }
         
         queue<int>q;
         int total = 0;
         for(int i=0;i<26;i++){
-            if(pa[i]){
+            if(paresent[i]){
                 total++;
                 if(indegree[i] == 0){
                     q.push(i);
                 }
             }
+            
         }
         string ans = "";
+        
         while(!q.empty()){
-            int node = q.front();
+            int top = q.front();
             q.pop();
-            ans.push_back(node+'a');
-            for(int i : adj[node]){
+            
+            ans.push_back(top + 'a');
+            for(int i : adj[top]){
                 indegree[i]--;
                 if(indegree[i] == 0){
                     q.push(i);
                 }
             }
         }
-        if(ans.size() != total){
-            return "";
-        }
-        return ans;
+        return ans.size() != total?"":ans;
+        
+        
+        
+        
         
         
     }
