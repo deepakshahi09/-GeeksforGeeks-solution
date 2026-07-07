dcaa52bbffb99dc900fb1da17557e8bf485e03ed
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-07-07 21:09:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Shortest Transformation Sequences in a Word List/shortest-transformation-sequences-in-a-word-list.cpp
+++ Difficulty: Hard/Shortest Transformation Sequences in a Word List/shortest-transformation-sequences-in-a-word-list.cpp
@@ -1,58 +1,53 @@
 class Solution {
   public:
     vector<vector<string>> findSequences(vector<string> &words, string &s, string &e) {
-            vector<vector<string>>ans;
-            queue<vector<string>>q;
-            vector<string>useOflevel;
-            int level = 0;
-            useOflevel.push_back(s);
-            q.push({s});
-            unordered_set<string>st(words.begin(),words.end());
-            
-            while(!q.empty()){
-                vector<string>res = q.front();
-                q.pop();
-                
-                
-                if(res.size() > level){
-                    level = res.size();
-                    for(string s : useOflevel){
-                        st.erase(s);
-                    }
-                    useOflevel.clear();
+        vector<vector<string>>ans;
+        queue<vector<string>>q;
+        unordered_set<string>st(words.begin(),words.end());
+        vector<string>levelOfview;
+        levelOfview.push_back(s);
+        int level = 0;
+        q.push({s});
+        
+        while(!q.empty()){
+            vector<string>res = q.front();
+            q.pop();
+            if(res.size() > level){
+                level = res.size();
+                for(string s : levelOfview){
+                    st.erase(s);
                 }
-                
-                
-                string word = res.back();
-                
-                if(word == e){
-                    if(ans.size() == 0){
-                        ans.push_back(res);
-                    }
-                    else if(ans[0].size() == res.size()){
-                        ans.push_back(res);
-                    }
-                }
-                
-                for(int i=0;i<word.size();i++){
-                    char original = word[i];
-                    for(char j='a';j<='z';j++){
-                        word[i] = j;
-                        if(st.count(word) > 0){
-                            useOflevel.push_back(word);
-                            res.push_back(word);
-                            q.push(res);
-                            res.pop_back();
-                        }
-                        
-                    }
-                    word[i]= original;
-                }
-                
-                
-                
+                levelOfview.clear();
             }
-            return ans;
+            string word = res.back();
+            if(word == e){
+                if(ans.size() == 0){
+                    ans.push_back(res);
+                }
+                else if(ans[0].size() == res.size()){
+                    ans.push_back(res);
+                }
+            }
+            
+            for(int i=0;i<word.size();i++){
+                char original = word[i];
+                for(char ch='a';ch<='z';ch++){
+                    word[i] = ch;
+                    if(st.count(word) > 0){
+                        res.push_back(word);
+                        levelOfview.push_back(word);
+                        q.push(res);
+                        res.pop_back();
+                    }
+                }
+                word[i] = original;
+            }
+            
+        }
+        return ans;
+        
+        
+        
         
     }
 };
