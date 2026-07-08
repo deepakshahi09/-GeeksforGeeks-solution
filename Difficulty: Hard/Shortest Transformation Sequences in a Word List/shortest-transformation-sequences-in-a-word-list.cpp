caa82808bf6ff869de353e05a0e573aeb022b454
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-07-08 10:18:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Shortest Transformation Sequences in a Word List/shortest-transformation-sequences-in-a-word-list.cpp
+++ Difficulty: Hard/Shortest Transformation Sequences in a Word List/shortest-transformation-sequences-in-a-word-list.cpp
@@ -1,24 +1,28 @@
 class Solution {
   public:
     vector<vector<string>> findSequences(vector<string> &words, string &s, string &e) {
+      
         vector<vector<string>>ans;
-        queue<vector<string>>q;
+        vector<string>levelofview;
         unordered_set<string>st(words.begin(),words.end());
-        vector<string>levelOfview;
-        levelOfview.push_back(s);
-        int level = 0;
+        queue<vector<string>>q;
         q.push({s});
+        levelofview.push_back(s);
+        int level = 0;
+        
         
         while(!q.empty()){
             vector<string>res = q.front();
             q.pop();
+            
             if(res.size() > level){
                 level = res.size();
-                for(string s : levelOfview){
-                    st.erase(s);
+                for(auto it:levelofview){
+                    st.erase(it);
                 }
-                levelOfview.clear();
+                levelofview.clear();
             }
+            
             string word = res.back();
             if(word == e){
                 if(ans.size() == 0){
@@ -28,26 +32,21 @@ class Solution {
                     ans.push_back(res);
                 }
             }
-            
             for(int i=0;i<word.size();i++){
                 char original = word[i];
-                for(char ch='a';ch<='z';ch++){
+                for(char ch = 'a';ch<='z';ch++){
                     word[i] = ch;
-                    if(st.count(word) > 0){
+                    if(st.count(word)>0){
+                        levelofview.push_back(word);
                         res.push_back(word);
-                        levelOfview.push_back(word);
                         q.push(res);
                         res.pop_back();
                     }
                 }
                 word[i] = original;
             }
-            
         }
         return ans;
-        
-        
-        
         
     }
 };
