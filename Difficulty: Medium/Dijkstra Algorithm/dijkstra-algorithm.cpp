b9f06d93b8bc4019d88acf1b19e9d15937a6a785
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-07-09 09:14:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -2,23 +2,18 @@ class Solution {
   public:
     vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
         vector<pair<int,int>>adj[V];
-        
-        for(auto &it : edges){
+        for(auto it : edges){
             int u = it[0];
             int v = it[1];
-            int wt = it[2];
-            
-            adj[u].push_back({v,wt});
-            adj[v].push_back({u,wt});
+            int w = it[2];
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
         }
-        
-            priority_queue<pair<int,int>,
-               vector<pair<int,int>>,
-               greater<pair<int,int>>> pq;
-               pq.push({0,src});
-        vector<int>dist(V,INT_MAX);
+        vector<int>dist(V,1e9);
+         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         
         dist[src] = 0;
+        pq.push({0,src});
         
         while(!pq.empty()){
             int node = pq.top().second;
@@ -30,12 +25,11 @@ class Solution {
                 int wt = it.second;
                 
                 if(dist[node] + wt < dist[adjnode]){
-                    dist[adjnode] = dist[node] + wt;
+                    dist[adjnode] = dist[node]+wt;
                     pq.push({dist[adjnode],adjnode});
                 }
             }
         }
         return dist;
-        
     }
 };
