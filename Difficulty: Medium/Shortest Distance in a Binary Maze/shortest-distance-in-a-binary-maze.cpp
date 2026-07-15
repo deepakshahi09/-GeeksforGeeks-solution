c81a21941583e049912820623a1a69d0897d5c5e
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-07-15 17:10:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.cpp
+++ Difficulty: Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.cpp
@@ -1,54 +1,44 @@
 class Solution {
-public:
+  public:
     int shortestPath(vector<vector<int>> &mat, vector<int> &src, vector<int> &dest) {
-        
+        queue<pair<int,pair<int,int>>>q;
         int n = mat.size();
         int m = mat[0].size();
-        
-        // If source or destination is blocked
-        if(mat[src[0]][src[1]] == 0 || mat[dest[0]][dest[1]] == 0)
+        if(mat[src[0]][src[1]] == 0 || mat[dest[0]][dest[1]] == 0){
             return -1;
-        
-        // If source and destination are the same
-        if(src[0] == dest[0] && src[1] == dest[1])
+        }
+        if(src[0] == dest[0] && src[1] == dest[1]){
             return 0;
-        
-        vector<vector<int>> dist(n, vector<int>(m, 1e9));
-        queue<pair<int, pair<int, int>>> q;
-        
+        }
+        vector<vector<int>>dist(n,vector<int>(m,1e9));
         dist[src[0]][src[1]] = 0;
-        q.push({0, {src[0], src[1]}});
+        q.push({0,{src[0],src[1]}});
         
-        int delrow[] = {-1, 0, 1, 0};
-        int delcol[] = {0, 1, 0, -1};
+        int delrow[] = {0,1,0,-1};
+        int delcol[] = {-1,0,1,0};
         
-        while(!q.empty()) {
-            
-            int dis = q.front().first;
+        while(!q.empty()){
+            int step = q.front().first;
             int row = q.front().second.first;
             int col = q.front().second.second;
             q.pop();
             
-            for(int i = 0; i < 4; i++) {
+            for(int i=0;i<4;i++){
+                int nrow = delrow[i]+row;
+                int ncol = delcol[i]+col;
                 
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-                
-                if(nrow >= 0 && nrow < n &&
-                   ncol >= 0 && ncol < m &&
-                   mat[nrow][ncol] == 1 &&
-                   dis + 1 < dist[nrow][ncol]) {
-                    
-                    dist[nrow][ncol] = dis + 1;
-                    
-                    if(nrow == dest[0] && ncol == dest[1])
-                        return dis + 1;
-                    
-                    q.push({dis + 1, {nrow, ncol}});
+                if(nrow < n && nrow >=0 && ncol < m && ncol >=0 &&
+                    mat[nrow][ncol]==1 && step+1 < dist[nrow][ncol]
+                ){
+                    dist[nrow][ncol] = step+1;
+                    q.push({step+1,{nrow,ncol}});
+                    if(nrow == dest[0] && ncol == dest[1]){
+                        return step+1;
+                    }
                 }
             }
         }
-        
         return -1;
+        
     }
 };
