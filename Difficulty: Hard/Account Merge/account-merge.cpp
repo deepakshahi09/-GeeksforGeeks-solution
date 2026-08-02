fce200ed11ebf0cee004e8251fb3c41309af28fd
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-08-02 11:41:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Account Merge/account-merge.cpp
@@ -0,0 +1,81 @@
+class DSU{
+    public:
+    vector<int>parent,rank;
+    DSU(int n){
+        parent.resize(n+1);
+        rank.resize(n+1,0);
+        for(int i=0;i<=n;i++){
+            parent[i] = i;
+        }
+    }
+    
+    int find(int u){
+        if(parent[u] == u) return u;
+        return parent[u] = find(parent[u]);
+    }
+    
+    void unionbyrank(int u, int v){
+        int pu = find(u);
+        int pv = find(v);
+        
+        if(pu == pv) return;
+        if(rank[pu] > rank[pv]){
+            parent[pv] = pu;
+        }
+        else if(rank[pv] > rank[pu]){
+            parent[pu] = pv;
+        }
+        else{
+            parent[pu] = pv;
+            rank[pv]++;
+        }
+    }
+    
+};
+
+class Solution {
+    
+  public:
+    vector<vector<string>> accMerge(vector<vector<string>>& arr) {
+        int n = arr.size();
+        DSU ds(n);
+        unordered_map<string,int>mp;
+        for(int i=0;i<n;i++){
+            for(int j=1;j<arr[i].size();j++){
+                string email = arr[i][j];
+                if(mp.find(email) == mp.end()){
+                    mp[email] = i;
+                }
+                else{
+                    ds.unionbyrank(i,mp[email]);
+                }
+            }
+        }
+        
+        vector<string>mermail[n];
+        for(auto it: mp){
+            string mail = it.first;
+            int node = ds.find(it.second); 
+            mermail[node].push_back(mail);
+        }
+        
+        vector<vector<string>>ans;
+        
+        for(int i=0;i<n;i++){
+            if(mermail[i].size()==0){
+                continue;
+            }
+            sort(mermail[i].begin(),mermail[i].end());
+            vector<string>temp;
+            temp.push_back(arr[i][0]);
+            for(string s : mermail[i]){
+                temp.push_back(s);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+        
+        
+        
+    }
+};
