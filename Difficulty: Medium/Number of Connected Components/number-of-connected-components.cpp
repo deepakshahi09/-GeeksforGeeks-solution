41d72d998354d6dbe1363c325f10c12a1c3f41f6
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-08-05 12:28:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Connected Components/number-of-connected-components.cpp
+++ Difficulty: Medium/Number of Connected Components/number-of-connected-components.cpp
@@ -1,33 +1,58 @@
+class DSU {
+public:
+    vector<int> parent, rank;
+
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    int findParent(int node) {
+        if (parent[node] == node)
+            return node;
+
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void Union(int u, int v) {
+        int pu = findParent(u);
+        int pv = findParent(v);
+
+        if (pu == pv)
+            return;
+
+        if (rank[pu] < rank[pv]) {
+            parent[pu] = pv;
+        }
+        else if (rank[pv] < rank[pu]) {
+            parent[pv] = pu;
+        }
+        else {
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+    }
+};
+
 class Solution {
-    private:
-        void dfs(int node, vector<int>&visit,vector<int>adj[]){
-            visit[node] = 1;
-            for(int i : adj[node]){
-                if(!visit[i]){
-                    dfs(i,visit,adj);
-                }
-            }
-        }
-  public:
+public:
     int countConnected(int V, vector<vector<int>>& edges) {
-        int n = edges.size();
-        vector<int>adj[V];
-        for(int i=0;i<n;i++){
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+
+        DSU dsu(V);
+
+        for (auto &it : edges) {
+            dsu.Union(it[0], it[1]);
         }
-        vector<int>visit(V,0);
-        int count = 0;
-        for(int i=0;i<V;i++){
-            if(!visit[i]){
-                count++;
-                dfs(i,visit,adj);
-            }
+
+        int cnt = 0;
+        for (int i = 0; i < V; i++) {
+            if (dsu.findParent(i) == i)
+                cnt++;
         }
-        return count;
-        
-        
+
+        return cnt;
     }
 };
