e6390a3d1ab4c7bae148316606a599c0df43ad85
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-08-06 14:50:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Shortest Transformation Length in a Word List/shortest-transformation-length-in-a-word-list.cpp
+++ Difficulty: Hard/Shortest Transformation Length in a Word List/shortest-transformation-length-in-a-word-list.cpp
@@ -2,32 +2,30 @@ class Solution {
   public:
     int wordLadder(vector<string> &words, string &s, string &e) {
         unordered_set<string>st(words.begin(),words.end());
-        queue<pair<string , int>>q;
-        
+        if(!st.count(e)){
+            return 0;
+        }
+        queue<pair<string,int>>q;
         q.push({s,1});
         
         while(!q.empty()){
-            auto[word,step]=q.front();
+            auto[word,step] = q.front();
             q.pop();
-            if(word == e){
-                return step;
-            }
+            
+            if(word == e) return step;
             for(int i=0;i<word.size();i++){
                 char original = word[i];
-                for(char ch ='a';ch<='z';ch++){
+                for(int ch='a';ch<='z';ch++){
                     word[i] = ch;
-                    if(st.count(word)>0){
+                    if(st.count(word)){
                         q.push({word,step+1});
                         st.erase(word);
                     }
-                    
                 }
                 word[i] = original;
             }
         }
         return 0;
-        
-        
         
     }
 };
