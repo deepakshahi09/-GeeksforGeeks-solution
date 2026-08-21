5fa8840202b3f20a085b8c5395b26e791041422c
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-08-21 15:04:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Longest Path in a Directed Acyclic Graph/longest-path-in-a-directed-acyclic-graph.cpp
+++ Difficulty: Hard/Longest Path in a Directed Acyclic Graph/longest-path-in-a-directed-acyclic-graph.cpp
@@ -7,18 +7,15 @@ class Solution {
 			int u = it[0];
 			int v = it[1];
 			int wt = it[2];
-			
 			adj[u].push_back({v, wt});
 			indegree[v]++;
 		}
-		
 		queue<int>q;
 		for (int i = 0; i<V; i++) {
 			if (indegree[i] == 0) {
 				q.push(i);
 			}
 		}
-		
 		vector<int>topo;
 		while (!q.empty()) {
 			int top = q.front();
@@ -32,23 +29,18 @@ class Solution {
 				}
 			}
 		}
-		
 		vector<int>dist(V, INT_MIN);
 		dist[src] = 0;
 		for (auto &node: topo) {
-			
 			if (dist[node] == INT_MIN) {
 				continue;
 			}
 			for (auto &i:adj[node]) {
 				int v = i.first;
 				int wt = i.second;
-				
 				dist[v] = max(dist[v], dist[node]+wt);
 			}
-			
 		}
 		return dist;
 	}
-	
 };
