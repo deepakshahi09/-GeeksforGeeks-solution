bfbce4dbd336510f1d2c902dd89ea4ebbd4e931f
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-09-02 12:20:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,65 +1,50 @@
-class DSU{
-    public:
-    vector<int>parent,rank;
-    
-    DSU(int n){
-        parent.resize(n+1);
-        rank.resize(n+1,0);
-        for(int i=0;i<=n;i++){
-            parent[i] = i;
-        }
-    }
-    
-    int find_p(int v){
-        if(parent[v] == v) return v;
-        return parent[v] = find_p(parent[v]);
-    }
-    
-    void union_by_rank(int u, int v){
-        int pu = find_p(u);
-        int pv = find_p(v);
-        
-        if(pu == pv) return;
-        if(rank[pu] > rank[pv]){
-            parent[pv] = pu;
-        }
-        else if(rank[pu] < rank[pv]){
-            parent[pu] = pv;
-        }
-        else{
-            parent[pu] = pv;
-            rank[pv]++;
-        }
-    }
-    
-};
-
 class Solution {
-  public:
+public:
     int spanningTree(int V, vector<vector<int>>& edges) {
-        vector<vector<int>>adj;
-        DSU ds(V);
-        for(auto &it: edges){
+
+        vector<vector<pair<int, int>>> adj(V);
+
+        for(auto it : edges) {
+
             int u = it[0];
             int v = it[1];
             int wt = it[2];
-            
-            if(u < v){
-                adj.push_back({wt,u,v});
+
+            adj[u].push_back({v, wt});
+            adj[v].push_back({u, wt});
+        }
+
+        priority_queue<
+            pair<int, int>,
+            vector<pair<int, int>>,
+            greater<pair<int, int>>
+        > pq;
+
+        vector<int> visit(V, 0);
+
+        pq.push({0, 0});
+
+        int ans = 0;
+
+        while(!pq.empty()) {
+
+            auto [weight, node] = pq.top();
+            pq.pop();
+
+            if(visit[node])
+                continue;
+
+            visit[node] = 1;
+            ans += weight;
+
+            for(auto it : adj[node]) {
+
+                if(!visit[it.first]) {
+                    pq.push({it.second, it.first});
+                }
             }
         }
-        sort(adj.begin(),adj.end());
-        int sum = 0;
-        for(auto &it : adj){
-            int wt = it[0];
-            int u = it[1];
-            int v = it[2];
-            if(ds.find_p(u) != ds.find_p(v)){
-                sum+=wt;
-                ds.union_by_rank(u,v);
-            }
-        }
-        return sum;
-        
+
+        return ans;
     }
 };
