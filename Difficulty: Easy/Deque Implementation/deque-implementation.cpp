4be8a17c54cb8b21f8a0b278ca0bbecd76e123b1
Deepak kumar <147906161+deepakshahi09@users.noreply.github.com>
2026-09-10 06:53:51 -0700

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Deque Implementation/deque-implementation.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    void pb(deque<int>& dq, int x) {
+        dq.push_back(x);
+        
+    }
+
+    void ppb(deque<int>& dq) {
+        if(!dq.empty()){
+            dq.pop_back();
+        }
+        // code here
+    }
+
+        
+    int front_dq(deque<int>& dq) {
+        if(!dq.empty()){
+            return dq.front();
+        }
+        return -1;
+    }
+        
+
+    void pf(deque<int>& dq, int x) {
+         dq.push_front(x);
+        // code here
+    }
+};
